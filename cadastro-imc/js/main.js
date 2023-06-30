15a92ad009f71b663075e4aa406c0f557293424d
12873838 <luizrobertohsilva@hotmail.com>
2023-06-30 19:21:51 -0300

    Add lista user

--- cadastro-imc/js/main.js
+++ cadastro-imc/js/main.js
@@ -11,23 +11,33 @@ class User {
 class Bd {
     constructor() {
         let id = localStorage.getItem('id');
-        
-        if(id === null) {
+        if (id === null) {
             localStorage.setItem('id', 0);
         }
     }
 
     getNextId() {
         let nextId = localStorage.getItem('id');
-        
         return parseInt(nextId) + 1;
     }
 
     saveData(user) {
         let id = this.getNextId();
-        
         localStorage.setItem(id, JSON.stringify(user));
         localStorage.setItem('id', id);
+    }
+
+    recuperationData() {
+        let users = [];
+        const id = localStorage.getItem('id')
+        
+        for (let i = 1; i <= id; i++) {
+            const userData = JSON.parse(localStorage.getItem(i));
+
+            if(userData === null) continue;
+            else users.push(userData);
+        }
+        return users;
     }
 
 }
@@ -35,15 +45,14 @@ class Bd {
 class Imc {
 
     constructor() {
-        this.btn = document.getElementById('btnForm');
         this.nameImput = document.getElementById('name');
         this.ageImput = document.getElementById('age');
         this.heightImput = document.getElementById('height');
         this.weightImput = document.getElementById('weight');
-        this.btn.addEventListener("click", () => this.sendFormorm());
     }
 
-    sendForm() {
+
+    registerForm() {
         const name = this.nameImput.value;
         const age = this.ageImput.value;
         const height = this.heightImput.value;
@@ -65,6 +74,7 @@ class Imc {
         }
     }
 
+
     cleanForm() {
         this.nameImput.value = "";
         this.ageImput.value = "";
@@ -78,10 +88,18 @@ class Imc {
     }
 
     listAll() {
-        this.users.forEach((user, index) => {
-            console.log(`Usuários ${index + 1}`)
-            console.log(`Nome: ${user.name}; Idade: ${user.age}; Altura: ${user.height}; Peso: ${user.weight}`);
-        })
+        let listUsers= BD.recuperationData()
+        let tbodyUser = document.getElementById('tbodyUser');
+        
+        listUsers.forEach(e => {
+            let row = tbodyUser.insertRow();
+            
+            row.insertCell(0).innerHTML = `${e.name}`;
+            row.insertCell(1).innerHTML = `${e.age}`;
+            row.insertCell(2).innerHTML = `${e.height}`;
+            row.insertCell(3).innerHTML = `${e.weight}`;
+            row.insertCell(4).innerHTML = `${e.imc}`;
+        });    
     }
 
     msgSendinData(msg, bgcolor) {
@@ -100,9 +118,27 @@ class Imc {
         }, 3000)
     }
 
-
 }
 const BD = new Bd();
-new Imc()
+const imc = new Imc();
+const pathName = () => {
+
+    if (document.location.pathname === '/index.html') {
+        window.addEventListener('load', () => {
+            imc.listAll();
+        });
+    }
+
+    if (document.location.pathname === '/cadastro.html') {
+        const btnForm = document.getElementById('btnForm');
+        btnForm.addEventListener('click', (event) => {
+            event.preventDefault(); // Impede o envio padrão do formulário
+            imc.registerForm();
+        });
+    }
+
+}
+pathName();
+
 
 
